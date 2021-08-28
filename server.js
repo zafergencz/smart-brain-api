30eddea103e2276626f49f8abc5130a992681e98
zafer genc <zafergenc5555@gmail.com>
2021-08-28 22:49:52 +0300

    port adjustment

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.post('/register', register.handleRegister(db,bcrypt));
 app.get('/profile/:id', (req,res) => {profile.handleProfile(req,res,db)} );
 app.put('/image', (req,res) => {image.handleImage(req,res,db)});
 app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)});
-app.listen(3000, () => {
-	console.log('app is running at port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running at port ${process.env.PORT}`);
 })
 
 /*
